3f08a5542e454d97ddfad1bfddbaa827bb4591ab
JALADISAI <104709845+JALADISAI@users.noreply.github.com>
2024-06-10 14:34:29 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import FormComponent from "../src/Components/FormComponent";
+import "../src/Components/FormComponent.css";
+
+function App() {
+  return (
+    <div className="App">
+      <FormComponent />
+    </div>
+  );
+}
+
+export default App;
